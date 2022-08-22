6ed7e6e949824673d88ba5cd87afab95babf88fa
Salvia <SalviaSage@users.noreply.github.com>
2022-08-22 03:08:57 -0400

    Update BlockCursorForOVR.py

--- PythonScript Plugin/BlockCursorForOVR.py
+++ PythonScript Plugin/BlockCursorForOVR.py
@@ -1,5 +1,2 @@
 ## Should make the blinker appear like a block when in OVR mode and not so when in INS mode.
-
-CARETSTYLE_OVERSTRIKE_BLOCK = 16
-editor1.setCaretStyle(CARETSTYLE_OVERSTRIKE_BLOCK)
-editor2.setCaretStyle(CARETSTYLE_OVERSTRIKE_BLOCK)
+editor.setCaretStyle(editor.getCaretStyle() | 16)
